9da89361252ef1415c7e2369acb8a274a87fd66b
Ashwin1729 <mathematicswroteus@gmail.com>
2023-06-28 00:25:04 +0530

    Header Layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: "#6AF2F0",
+        cyanHover: "#04ECF0",
+      },
+    },
   },
   plugins: [],
 };
